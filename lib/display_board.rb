c0cf239d3a9b18a119254a4edc61f096ddf584c4
Kayla Cross <kac288@georgetown.edu>
2018-01-26 15:25:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-  2.times {print "   |"}
-  puts "   "
-  puts "-----------"
-  2.times {print "   |"}
-  puts "   "
-  puts "-----------"
-  2.times {print "   |"}
-  puts "   "
-end
-
-def display_board_test
   2.times{
     2.times {print "   |"}
     puts "   "
@@ -20,4 +9,4 @@ def display_board_test
   puts "   "
 end
 
-display_board_test
+display_board
